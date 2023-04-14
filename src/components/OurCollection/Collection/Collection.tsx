33550077a3c2202f898c11b293bfd769bb716f13
Samuel Santos <samuka0102128@gmail.com>
2023-04-14 18:45:23 -0300

    Our Collection section done

--- src/components/OurCollection/Collection/Collection.tsx
+++ src/components/OurCollection/Collection/Collection.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import image from '../../../assets/3dApe1.jpg'
+import image1 from '../../../assets/3dApe1.jpg'
+import image2 from '../../../assets/3dApe2.jpg'
+import image3 from '../../../assets/3dApe3.jpg'
 
 export const Collection = () => {
+  const [image, setImage] = React.useState('first')
+
+
   return (
     <div>
-      <img src={image} alt="Bored Ape 3d" />
+      {image === 'first' && (
+        <div className='rounded-lg overflow-hidden'>
+          <img src={image1} alt="Bored Ape 3d" />
+        </div>
+      )}
+      {image === 'second' && (
+        <div className='rounded-lg overflow-hidden'>
+          <img src={image2} alt="Bored Ape 3d" />
+        </div>
+        
+      )}
+      {image === 'third' && (
+        <div className='rounded-lg overflow-hidden'>
+          <img src={image3} alt="Bored Ape 3d"/>
+        </div>
+        
+      )}
+
+      <section className='flex items-center justify-center gap-4 my-12'>
+        <div className={`rounded-full ${image === 'first' ? 'bg-black' : 'bg-white'} w-12 h-12 cursor-pointer`} onClick={handleClick => setImage('first')}></div>
+
+
+        <div className={`rounded-full ${image === 'second' ? 'bg-black' : 'bg-white'} w-12 h-12 cursor-pointer `} onClick={handleClick => setImage('second')}></div>
+
+
+        <div className={`rounded-full ${image === 'third' ? 'bg-black' : 'bg-white'} w-12 h-12 cursor-pointer`} onClick={handleClick => setImage('third')}></div>
+      </section>
     </div>
   )
 }
--- src/components/OurCollection/OurCollection.tsx
+++ src/components/OurCollection/OurCollection.tsx
@@ -6,10 +6,10 @@ export const OurCollection = () => {
   return (
     <section className="flex flex-col justify-center items-center bg-[url('/src/assets/backgroundGreen.png')] mt-24 ">
       <OurCollectionHeader />
-      <section className="mt-12">
-        <div className='flex justify-around gap-24 h-24'>
-          <h1 className='text-6xl font-robotoCondensed font-bold'>Our Collection</h1>
-          <p className='text-2xl font-overpass self-end '>
+      <section className="my-12">
+        <div className='flex md:justify-around gap-4 flex-wrap justify-center md:gap-24 h-auto md:h-24'>
+          <h1 className='md:text-6xl text-4xl font-robotoCondensed font-bold'>Our Collection</h1>
+          <p className='md:text-2xl text-xl font-overpass self-end md:text-left text-center'>
             Ready to join the New Year Ape <br /> World? Browse our collections below
           </p>
         </div>
